0588902221cc0fbd7764f6af768149d3d442d8bd
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-11-15 16:13:41 +0200

    seo

--- js/totop.js
+++ js/totop.js
@@ -1,14 +1,1 @@
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
-window.addEventListener('scroll', function () {
-  if (window.scrollY > 200) {
-    scrollToTopBtn.style.display = 'flex';
-  } else {
-    scrollToTopBtn.style.display = 'none';
-  }
-});
-scrollToTopBtn.addEventListener('click', function () {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-});
+const scrollToTopBtn=document.getElementById("scrollToTopBtn");window.addEventListener("scroll",(function(){window.scrollY>200?scrollToTopBtn.style.display="flex":scrollToTopBtn.style.display="none"})),scrollToTopBtn.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
